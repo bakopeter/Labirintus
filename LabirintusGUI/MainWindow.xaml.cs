da4403a06828da456ebf619ef782d242f320db3d
bakopeter <bakopeter71@gmail.com>
2024-04-15 04:13:10 +0200

    CreateLab  function

--- LabirintusGUI/MainWindow.xaml.cs
+++ LabirintusGUI/MainWindow.xaml.cs
@@ -16,9 +16,50 @@ namespace LabirintusGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        int oszlopokSzama;
+        int sorokSzama;
+        public int OszlopokSzama { get => oszlopokSzama; set => oszlopokSzama = value; }
+        public int SorokSzama { get => sorokSzama; set => sorokSzama = value; }
+        
         public MainWindow()
         {
             InitializeComponent();
+            for (int i = 5; i <= 20; i++)
+            {
+                SorokMaxSzama.Items.Add(i);
+                OszlopokMaxSzama.Items.Add(i);
+            }
+            SorokMaxSzama.SelectedItem = 12;
+            OszlopokMaxSzama.SelectedItem = 12;
+            for (int i = 1; i <= 16; i++) { AllomanyIndex.Items.Add(i); }
+            AllomanyIndex.SelectedItem = 3 ;
+        }
+
+        private void LetrehozLab_Click(object sender, RoutedEventArgs e)
+        {
+            LabTer.Children.Clear();
+
+            SorokSzama = (int)SorokMaxSzama.SelectedItem;
+            OszlopokSzama = (int)OszlopokMaxSzama.SelectedItem;
+            for (int i = 1; i <= SorokSzama; i++)
+            {
+                for (int j = 1; j <= OszlopokSzama; j++)
+                {
+                    CheckBox labMezo = new CheckBox();
+                    labMezo.Width = 18; labMezo.Height = 18;
+                    LabTer.Children.Add(labMezo);
+                    labMezo.Margin = new Thickness((j-1) * 18, (i-1) * 18, 0, 0);
+
+                    if (i == 1 || i == SorokSzama || j == 1 || j == OszlopokSzama)
+                    {
+                        labMezo.IsEnabled = false;
+                        if ((i != SorokSzama - 1 && j !=1) || (i != 2 && j != OszlopokSzama)) 
+                        { 
+                            labMezo.IsChecked = true; 
+                        }
+                    }
+                }
+            }
         }
     }
 }
